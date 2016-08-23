d2c0687a74185f117cf37ee4ad0d9eb953cb13d1
Yun Lin <llmhyy@gmail.com>
2016-08-23 21:57:53 +0800

    minor

--- DebugExperiment/src/exp2/LongestConsecutiveSequenceDebug.java
+++ DebugExperiment/src/exp2/LongestConsecutiveSequenceDebug.java
@@ -8,7 +8,7 @@ public class LongestConsecutiveSequenceDebug {
 	 */
 	public static void main(String[] args){
 		LongestConsecutiveSequence longestConsecutiveSequence = new LongestConsecutiveSequence();
-        int num[] = {99, 1, 2, 100, 3, 2, 88, 4};
+        int num[] = {99, 1, 2, 100, 3, 2, 88, 4, 33, 34, 66, 67, 68, 11, 12, 13};
         int length = longestConsecutiveSequence.longestConsecutive(num);
         System.out.println(length==4);
     }
